0cfead1b0d2f7f3ec24b91403d6e71c5bc0e6372
AnuCod3 <anuprivat45@gmail.com>
2023-11-23 21:41:41 +0100

    DB Eingerichtet - Connection hergestellt - User Daten setzen und getten

--- public/index.js
+++ public/index.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
-import { getAnalytics } from "firebase/analytics";
+import {initializeApp} from "firebase/app";
+import {getDatabase, ref, set, get, child} from "firebase/database";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -13,9 +13,50 @@ const firebaseConfig = {
     storageBucket: "kibby-2cd8c.appspot.com",
     messagingSenderId: "453662012145",
     appId: "1:453662012145:web:226ec559285a1fd0882f50",
-    measurementId: "G-J1TMSVRZ0S"
+    measurementId: "G-J1TMSVRZ0S",
+    databaseURL: "https://kibby-2cd8c-default-rtdb.europe-west1.firebasedatabase.app"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-// analytics = getAnalytics(app);
+const db = getDatabase();
+
+function writeUserData(userId, name, email, imageUrl) { // setUserData
+
+    const reference = ref(db, 'users/' + userId);
+
+    set(reference, {
+        username: name, email: email, profile_picture: imageUrl
+    });
+}
+
+/*const dbRef = ref(getDatabase());
+get(child(dbRef, `users/`)).then((snapshot) => { // getAllData
+    if (snapshot.exists()) {
+        console.log(snapshot.val());
+    } else {
+        console.log("No data available");
+    }
+}).catch((error) => {
+    console.error(error);
+});*/
+
+function getUserData(userId) { // getUserData
+
+    const reference = ref(db, 'users/' + userId);
+
+    get(child(dbRef, `users/` + userId)).then((snapshot) => { // getData
+        if (snapshot.exists()) {
+            console.log(snapshot.val());
+        } else {
+            console.log("No data available");
+        }
+    }).catch((error) => {
+        console.error(error);
+    });
+}
+
+
+writeUserData("1", "Kiliann", "kilian@hnbk-schule.de", "myimageurl");
+writeUserData("2", "penis", "kilian@hnbk-schule.de", "myimageurl");
+getUserData("2");
